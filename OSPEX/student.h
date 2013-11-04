b1c364f76bdad64086205743d3473cb13840663e
Taylor Sallee <taylor.sallee@wl-dhcp207-215.mines.edu>
2013-11-03 21:02:21 -0700

    added full path of /usr/src/include/printmessage.h to student.h

--- OSPEX/student.h
+++ OSPEX/student.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <minix/pi.h>
-#include </usr/src/printmessage.h>
+#include </usr/src/include/printmessage.h>
 #include "com.h"
 #include "glo.h"
 #include "procs.h"
