841898a13eb417fb6329a1edfd1183fb4a98ba17
Taylor Sallee <taylor.sallee@wl-dhcp207-215.mines.edu>
2013-11-03 23:08:42 -0700

    removed task call from testCall.c, moved it to misc.c

--- OSPEX/testCall.c
+++ OSPEX/testCall.c
@@ -1,10 +1,8 @@
 #include <printmessage.h>
 #include <obtain_proctable.h>
-#include <sched_task_call.h>
 #include <stdio.h>
 int main(int argc, char** args) {
 	int a = printmessage();
 	int b = obtain_proctable();
-	int c = sched_task_call();
 	return 0;
 }
