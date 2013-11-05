b4a8e18f75007b83a11987e51faa2f933ec16fdb
Taylor Sallee <taylor.sallee@wl-dhcp197-160.mines.edu>
2013-11-05 15:29:31 -0700

    Removed unneccesary print statements

--- OSPEX/testCall.c
+++ OSPEX/testCall.c
@@ -1,9 +1,7 @@
 #include "student.h"
 #include <stdio.h>
 int main(int argc, char** args) {
-	int a = printmessage();
 	struct pi p;
-	int b = obtain_proctable((char*) &p);
-
+	int i = obtain_proctable((char*) &p);
 	return 0;
 }
