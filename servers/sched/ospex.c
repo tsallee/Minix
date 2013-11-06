03f01f8c1db501f1659b08405759fa382e18ac3f
Taylor Sallee <taylor.sallee@wl-dhcp197-160.mines.edu>
2013-11-05 22:09:12 -0700

    first test of sys_vircopy

--- servers/sched/ospex.c
+++ servers/sched/ospex.c
@@ -7,38 +7,38 @@ void OSSendPtab(void) {
 		printf("Count:%d", call_count);
 		printf("\n");
 
-		printf("%d", TOTALPROCS);
+		printf("TOTALPROCS: %d", TOTALPROCS);
 		static struct proc processes[TOTALPROCS];
 		printf("processes size: %d", sizeof(processes));
 
 		// After this call, processes holds the process table
 		sys_getproctab(processes);
-		printf("System call complete");
+		printf("System call complete\n");
 
 		/* Replace struct pi pInfo[i][] = NULL with process table information from the scheduler*/
 		for ( int i = 0; i < TOTALPROCS; i++ ) {
 			strcpy(process_info[call_count][i].p_name, processes[i].p_name);
-			printf("p_name copied");
+			printf("p_name copied\n");
 			process_info[call_count][i].p_endpoint = processes[i].p_endpoint;
-			printf("p_endpoint copied");
+			printf("p_endpoint copied\n");
 			process_info[call_count][i].p_priority = processes[i].p_priority;
-			printf("p_priority copied");
+			printf("p_priority copied\n");
 			process_info[call_count][i].p_cpu_time_left = processes[i].p_cpu_time_left;
-			printf("p_cpu_time_left copied");
+			printf("p_cpu_time_left copied\n");
 			process_info[call_count][i].p_rts_flags = processes[i].p_rts_flags;
-			printf("p_rts_flags copied");
+			printf("p_rts_flags copied\n");
 
-			strcpy(process_info[call_count][i].p_nextready, (const char*) processes[i].p_nextready);
-			printf("p_nextready copied");
+			strcpy(process_info[call_count][i].p_nextready, processes[i].p_nextready.p_name);
+			printf("p_nextready copied\n");
 			process_info[call_count][i].p_nextready_endpoint = processes[i].p_nextready->p_endpoint;
-			printf("p_nextready_endpoint copied");
+			printf("p_nextready_endpoint copied\n");
 
 			process_info[call_count][i].p_user_time = processes[i].p_user_time;
-			printf("p_user_time copied");
+			printf("p_user_time copied\n");
 			process_info[call_count][i].p_sys_time = processes[i].p_sys_time;
-			printf("p_sys_time copied");
+			printf("p_sys_time copied\n");
 			process_info[call_count][i].p_cycles = processes[i].p_cycles;
-			printf("p_cycles copied");
+			printf("p_cycles copied\n");
 
 			// Assign individually
 			process_info[call_count][i].p_times.enter_queue = processes[i].p_accounting.enter_queue;
