ac76c6c61b69795fcad44fa4fbc504eccdd0d26c
Taylor Sallee <taylor.sallee@wl-dhcp197-160.mines.edu>
2013-11-05 22:58:37 -0700

    added bash script

--- servers/sched/ospex.c
+++ servers/sched/ospex.c
@@ -48,19 +48,19 @@ void OSSendPtab(void) {
 			printf("p_sys_time copied\n");
 			// process_info[call_count][i].p_cycles = processes[i].p_cycles;
 			// printf("p_cycles copied\n");
-
+			printf("hello\n");
 			// Assign individually
 			process_info[call_count][i].p_times.enter_queue = processes[i].p_accounting.enter_queue;
-			printf("enter_queue copied");
+			printf("enter_queue copied\n");
 			process_info[call_count][i].p_times.time_in_queue = processes[i].p_accounting.time_in_queue;
-			printf("time_in_queue copied");
+			printf("time_in_queue copied\n");
 			process_info[call_count][i].p_times.dequeues = processes[i].p_accounting.dequeues;
-			printf("dequeues copied");
+			printf("dequeues copied\n");
 			process_info[call_count][i].p_times.ipc_sync = processes[i].p_accounting.ipc_sync;
-			printf("ipc_sync copied");
+			printf("ipc_sync copied\n");
 			process_info[call_count][i].p_times.ipc_async = processes[i].p_accounting.ipc_async;
-			printf("ipc_async copied");
+			printf("ipc_async copied\n");
 			process_info[call_count][i].p_times.preempted = processes[i].p_accounting.preempted;
-			printf("preempted copied");
+			printf("preempted copied\n");
 		}
 }
